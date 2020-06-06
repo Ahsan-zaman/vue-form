75c9f79fa3ecd75b3381ccfa31aa03b5c53f3995
Ahsan <12345jitu67890@gmail.com>
2020-06-06 16:20:49 +0300

    First init

---
+++ src/components/MainForm.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="form">
+    <p>
+        Form show up ?
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Form',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ src/views/Main.vue
@@ -0,0 +1,14 @@
+<template>
+  <main-form/>
+</template>
+
+<script>
+// @ is an alias to /src
+import MainForm from '@/components/MainForm.vue'
+
+export default {
+  components: {
+    MainForm
+  }
+}
+</script>
